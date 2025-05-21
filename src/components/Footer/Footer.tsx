11868f196437dbeb09260eaa017acd88d9f1a18e
Ilya Prozorov <prozorov.ilya2016@gmail.com>
2025-05-21 16:02:48 +0300

    Изменения адреса

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -27,7 +27,7 @@ export const Footer = () => {
           <p className="footer-contact">Контакты</p>
           <p className="footer-adress">Адрес:</p>
           <p className="footer-adress-city">г.Ижевск деревня пирогово</p>
-          <p className="footer-adress-street">ул.Пушкина д.130</p>
+          <p className="footer-adress-street">ул. Азина 12</p>
           <a href="tel:+57-61-75" className="footer-tel">
             57-61-75
           </a>
