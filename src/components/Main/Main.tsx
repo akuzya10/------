1ca72d7fe635a427f91ec7098fad045c35e3f83c
Ilya Prozorov <prozorov.ilya2016@gmail.com>
2025-05-27 17:46:54 +0300

    изменения индекса для меню

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -9,8 +9,7 @@ import { List } from "../List/List";
 
 const Main = () => {
   return (
-    <div className="main">
-      <img src={mainImage} alt="" className="main-image" />
+    <>
       {IS_MOBILE ? (
         <Header />
       ) : (
@@ -18,56 +17,59 @@ const Main = () => {
           <Header />
         </Container>
       )}
+      <div className="main">
+        <img src={mainImage} alt="" className="main-image" />
 
-      <Container>
-        <div className="main-content_wrapper">
-          <div className="main-content_blok">
-            <h1 className="main-content_blok-title">
-              ПРАВИЛЬНАЯ {IS_MOBILE && <br />} БАНЯ
-            </h1>
-            {!IS_MOBILE && (
-              <div className="main-content_blok-subtitle_wrapper">
-                <h1 className="main-content_blok-subtitle">БАРИНЪ</h1>
+        <Container>
+          <div className="main-content_wrapper">
+            <div className="main-content_blok">
+              <h1 className="main-content_blok-title">
+                ПРАВИЛЬНАЯ {IS_MOBILE && <br />} БАНЯ
+              </h1>
+              {!IS_MOBILE && (
+                <div className="main-content_blok-subtitle_wrapper">
+                  <h1 className="main-content_blok-subtitle">БАРИНЪ</h1>
 
-                <Logo />
-              </div>
-            )}
-            {IS_MOBILE && (
-              <>
-                <div className="main-content_blok-list">
-                  <List width={178} height={130} />
+                  <Logo />
                 </div>
-                <div className="main-content_blok-list">
-                  <List width={158} height={110} />
-                </div>
-                <div className="main-content_blok-list-too">
-                  <List width={158} height={110} />
-                </div>
+              )}
+              {IS_MOBILE && (
+                <>
+                  <div className="main-content_blok-list">
+                    <List width={178} height={130} />
+                  </div>
+                  <div className="main-content_blok-list">
+                    <List width={158} height={110} />
+                  </div>
+                  <div className="main-content_blok-list-too">
+                    <List width={158} height={110} />
+                  </div>
 
-                <div className="main-content_blok-list-three">
-                  <List width={158} height={110} />
-                </div>
-                <div className="main-content_blok-list-foo">
-                  <List width={158} height={110} />
-                </div>
-                <div className="main-content_blok-phone-wrapper">
-                  <a href="tel:+516175" className="main-content_blok-phone">
-                    Позвонить
-                  </a>
-                </div>
-              </>
-            )}
+                  <div className="main-content_blok-list-three">
+                    <List width={158} height={110} />
+                  </div>
+                  <div className="main-content_blok-list-foo">
+                    <List width={158} height={110} />
+                  </div>
+                  <div className="main-content_blok-phone-wrapper">
+                    <a href="tel:+516175" className="main-content_blok-phone">
+                      Позвонить
+                    </a>
+                  </div>
+                </>
+              )}
+            </div>
+
+            <div className="main-content_blok-small">
+              <h2 className="main-content_blok-small-title">
+                Правильные бани - это те в которых предоставлены все условия для
+                отличного отдыха и время проведённого без забот.
+              </h2>
+            </div>
           </div>
-
-          <div className="main-content_blok-small">
-            <h2 className="main-content_blok-small-title">
-              Правильные бани - это те в которых предоставлены все
-              условия для отличного отдыха и время проведённого без забот.
-            </h2>
-          </div>
-        </div>
-      </Container>
-    </div>
+        </Container>
+      </div>
+    </>
   );
 };
 
